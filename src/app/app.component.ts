8f8aab07cfcbfa4a36fda4928340b11bbaca14f2
Shubham-chandurkar <newshubhammm@gmail.com>
2021-01-09 17:42:40 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import arraywords from "../util/words";
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +9,34 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'word-generator';
+
+
+  words = '';
+  random = '';
+  limit = 1;
+
+  array: any[] = arraywords;
+
+  handleSlideChange(newLimit: number) {
+    this.limit = newLimit;
+  }
+
+  generate() {
+    this.words = arraywords.slice(0, this.limit).join(" ");
+  }
+
+  temp() {
+    var item = this.array[Math.floor(Math.random() * this.array.length)];
+    return item;
+  }
+
+  randomgenerate() {
+    this.random = "";
+    for (let i = 0; i < this.limit; i++) {
+      this.random += this.temp() + " ";
+    }
+  }
+
+
+
 }
